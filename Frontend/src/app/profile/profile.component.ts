cb67f73f2b3d94675cdf57d1dcc7f688a126c5e2
Łukasz Pisarczyk <lukasz.pisarczyk02@gmail.com>
2024-03-12 14:40:59 +0100

    add more tabs for examination results

--- Frontend/src/app/profile/profile.component.ts
+++ Frontend/src/app/profile/profile.component.ts
@@ -17,11 +17,15 @@ export class ProfileComponent implements OnInit, OnDestroy, AfterViewInit {
   examinationResults: any[] = [];
   usgResults = new MatTableDataSource<any>([]);
   rentgenResults = new MatTableDataSource<any>([]);
+  tkResults = new MatTableDataSource<any>([]);
+  mriResults = new MatTableDataSource<any>([]);
   private unsubscribe$ = new Subject<void>();
   displayedColumns: string[] = ['title', 'description', 'examinationDate', 'doctor', 'download'];
 
   @ViewChild('usgPaginator', {static: false}) usgPaginator!: MatPaginator;
   @ViewChild('rentgenPaginator', {static: false}) rentgenPaginator!: MatPaginator;
+  @ViewChild('tkPaginator', {static: false}) tkPaginator!: MatPaginator;
+  @ViewChild('mriPaginator', {static: false}) mriPaginator!: MatPaginator;
 
   constructor(private storageService: StorageService, private examinationResultService: ExaminationService, private userService: UserService) {
   }
@@ -41,6 +45,8 @@ export class ProfileComponent implements OnInit, OnDestroy, AfterViewInit {
   ngAfterViewInit() {
     this.usgResults.paginator = this.usgPaginator;
     this.rentgenResults.paginator = this.rentgenPaginator;
+    this.tkResults.paginator = this.tkPaginator;
+    this.mriResults.paginator = this.mriPaginator;
   }
 
   loadExaminationResults(): void {
@@ -65,16 +71,26 @@ export class ProfileComponent implements OnInit, OnDestroy, AfterViewInit {
 
         this.usgResults.data = data.filter((result: { type: string; }) => result.type === 'USG');
         this.rentgenResults.data = data.filter((result: { type: string; }) => result.type === 'XRAY');
+        this.tkResults.data = data.filter((result: { type: string; }) => result.type === 'CT'); // Nowa linia dla TK
+        this.mriResults.data = data.filter((result: { type: string; }) => result.type === 'MRI'); // Nowa linia dla MRI
         console.log(data);
 
         this.usgResults.paginator = this.usgPaginator;
         this.rentgenResults.paginator = this.rentgenPaginator;
+        this.tkResults.paginator = this.tkPaginator;
+        this.mriResults.paginator = this.mriPaginator;
 
         if (this.usgPaginator) {
           this.usgPaginator.firstPage();
         }
         if (this.rentgenPaginator) {
           this.rentgenPaginator.firstPage();
+        }
+        if (this.tkPaginator) {
+          this.tkPaginator.firstPage();
+        }
+        if (this.mriPaginator) {
+          this.mriPaginator.firstPage();
         }
       },
       error => {
